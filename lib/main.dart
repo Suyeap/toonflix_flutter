849d345893aa4b600d1c86f836603e786b0c04ce
suyeah <sy.han2@okestro.com>
2023-06-22 16:34:29 +0900

    timer add reset btn code challenge

--- lib/main.dart
+++ lib/main.dart
@@ -13,13 +13,15 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: ThemeData(
-            colorScheme: ColorScheme.fromSwatch(
-              backgroundColor: const Color(0xFFE7626C),
-            ),
-            textTheme: const TextTheme(
-              displayLarge: TextStyle(color: Color(0xFF232B55)),
-            ),
-            cardColor: const Color(0xFFF4EDDB)),
+          colorScheme: ColorScheme.fromSwatch(
+            backgroundColor: const Color(0xFF80bddc),
+          ),
+          textTheme: const TextTheme(
+            displayLarge: TextStyle(color: Color(0xFF43545c)),
+          ),
+          cardColor: const Color(0xFFF4EDDB),
+          secondaryHeaderColor: const Color(0xFFbcdced),
+        ),
         home: const HomeScreen());
   }
 }
--- lib/screens/home_screem.dart
+++ lib/screens/home_screem.dart
@@ -10,15 +10,69 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int totalSeconds = 1500;
+  static const setCount = 1500;
+  int totalSeconds = setCount;
+  bool isRunning = false;
   late Timer timer; // 사용 전에 무조건 초기화 하겠다 -> 초기화 later
+  int totalPomodoros = 0;
+
+  // setState() 함수 저장
+  void runOrPause() {
+    setState(() {
+      isRunning == true ? isRunning = false : isRunning = true;
+      // print('isRunning = $isRunning');
+    });
+  }
+
+  // 타이머 초기화 버튼 함수
+  void resetTimer() {
+    setState(() {
+      isRunning = false;
+      totalSeconds = setCount;
+    });
+    timer.cancel();
+  }
+
   void onTick(Timer timer) => setState(() {
-        totalSeconds -= 1;
+        if (totalSeconds == 0) {
+          // reset all
+          setState(() {
+            totalPomodoros += 1;
+            isRunning = false;
+            totalSeconds = setCount;
+          });
+          timer.cancel();
+        } else {
+          setState(() {
+            totalSeconds -= 1;
+          });
+        }
       });
-  void onStartPressed() => timer = Timer.periodic(
-        const Duration(seconds: 1),
-        onTick,
-      ); // onTick() 이렇게 호출하면 즉시 호출이니까 하지말기
+
+  // 시작 버튼 클릭시
+  void onStartPressed() {
+    timer = Timer.periodic(
+      const Duration(seconds: 1),
+      onTick, // onTick() 이렇게 호출하면 즉시 호출이니까 하지말기
+    );
+
+    runOrPause();
+  }
+
+  // 일시정지 버튼 클릭시
+  void onPausedPressed() {
+    timer.cancel(); // stop timer
+
+    runOrPause();
+  }
+
+  // 분 단위 포맷
+  String format(int seconds) {
+    var timeFormat =
+        Duration(seconds: seconds).toString().split(".").first.substring(2, 7);
+    return timeFormat;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,11 +80,12 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Column(
         children: [
           Flexible(
+            // TIMER
             flex: 1, // 화면의 비율
             child: Container(
               alignment: Alignment.bottomCenter,
               child: Text(
-                '$totalSeconds',
+                format(totalSeconds),
                 style: TextStyle(
                   color: Theme.of(context).cardColor,
                   fontSize: 70,
@@ -40,18 +95,34 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ),
           Flexible(
+            // PLYA-PAUSE BUTTON
             flex: 3,
-            child: Center(
-              child: IconButton(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
                   iconSize: 110,
-                  onPressed: onStartPressed, // not set yet
+                  onPressed: isRunning ? onPausedPressed : onStartPressed,
                   icon: Icon(
-                    Icons.play_circle_fill_rounded,
+                    isRunning
+                        ? Icons.pause_circle_filled_rounded
+                        : Icons.play_circle_fill_rounded,
                     color: Theme.of(context).cardColor,
-                  )),
+                  ),
+                ),
+                IconButton(
+                    // RESET BUTTON
+                    iconSize: 50,
+                    onPressed: resetTimer,
+                    icon: Icon(
+                      Icons.restore_rounded,
+                      color: Theme.of(context).secondaryHeaderColor,
+                    ))
+              ],
             ),
           ),
           Flexible(
+            // BOTTOM COUNT
             flex: 1,
             child: Row(
               children: [
@@ -74,7 +145,7 @@ class _HomeScreenState extends State<HomeScreen> {
                               fontWeight: FontWeight.w600),
                         ),
                         Text(
-                          '0',
+                          '$totalPomodoros',
                           style: TextStyle(
                               fontSize: 45,
                               color: Theme.of(context)
