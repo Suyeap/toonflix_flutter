3768b90af7020b86899ed68d468a3fbb38abd4f8
suyeah <sy.han2@okestro.com>
2023-06-23 18:15:01 +0900

    future class

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/models/webtoon_model.dart';
+import 'package:toonflix/services/api_service.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<WebtoonModel> webtoons = [];
+  bool isLoading = true;
+
+  void waitForWebtoons() async {
+    webtoons = await ApiService.getTodaysToons();
+    isLoading = false;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    waitForWebtoons();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print(webtoons);
+    print(isLoading);
     return Scaffold(
       backgroundColor: const Color(0xFFE8F6F3),
       appBar: AppBar(
@@ -12,7 +36,7 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: const Color(0xFFA2D9CE),
         foregroundColor: const Color(0xFF138D75),
         title: const Text(
-          'Today\'s 툰s',
+          'Today\'s toonz',
           style: TextStyle(fontWeight: FontWeight.w600),
         ),
       ),
