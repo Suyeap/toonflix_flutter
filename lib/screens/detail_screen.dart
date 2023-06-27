b79f1f31d343d0b215921f0fad1819ae6d9b4660
suyeah <sy.han2@okestro.com>
2023-06-27 18:55:20 +0900

    detail pages , episodes , info.plist setting

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/models/webtoon_detail_model.dart';
+import 'package:toonflix/models/webtoon_episode_model.dart';
+import 'package:toonflix/services/api_service.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
 
   const DetailScreen(
       {super.key, required this.title, required this.thumb, required this.id});
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  late Future<WebtoonDetailModel> webtoon;
+  late Future<List<WebtoonEpisodeModel>> episodes;
+  @override
+  void initState() {
+    super.initState();
+    webtoon = ApiService.getToonById(widget.id);
+    episodes = ApiService.getLatestEpisodesById(widget.id);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,45 +32,123 @@ class DetailScreen extends StatelessWidget {
         backgroundColor: const Color(0xFFA2D9CE),
         foregroundColor: const Color(0xFF138D75),
         title: Text(
-          title,
+          widget.title,
           style: const TextStyle(fontWeight: FontWeight.w600),
         ),
       ),
-      body: Column(children: [
-        const SizedBox(
-          height: 40,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Hero(
-              // Hero()로 컨테이너 감싸고 tag 동일 고유값 지정
-              tag: id,
-              child: Container(
-                width: 220,
-                clipBehavior: Clip.hardEdge,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 13,
-                      offset: const Offset(5, 5),
-                      color: Colors.black.withOpacity(0.3),
-                    )
-                  ],
+      body: SingleChildScrollView(
+        // SingleChildScrollView: overflow 제어
+        child: Padding(
+          padding: const EdgeInsets.all(40), // padding 위아래 끝부분 공백
+          child: Column(children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Hero(
+                  // Hero()로 컨테이너 감싸고 tag 동일 고유값 지정
+                  tag: widget.id,
+                  child: Container(
+                    width: 220,
+                    clipBehavior: Clip.hardEdge,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 13,
+                          offset: const Offset(5, 5),
+                          color: Colors.black.withOpacity(0.3),
+                        )
+                      ],
+                    ),
+                    child: Image.network(
+                      widget.thumb,
+                      headers: const {
+                        "user-agent":
+                            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+                      },
+                    ),
+                  ),
                 ),
-                child: Image.network(
-                  thumb,
-                  headers: const {
-                    "user-agent":
-                        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
-                  },
-                ),
-              ),
+              ],
             ),
-          ],
+            const SizedBox(
+              height: 20,
+            ),
+            FutureBuilder(
+              future: webtoon,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        snapshot.data!.about,
+                        style: const TextStyle(
+                          fontSize: 17,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        '${snapshot.data!.genre} / ${snapshot.data!.age}',
+                        style: const TextStyle(
+                          fontSize: 17,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      FutureBuilder(
+                        future: episodes,
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            return Column(
+                              children: [
+                                for (var e in snapshot.data!)
+                                  Container(
+                                    margin: const EdgeInsets.only(bottom: 10),
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(25),
+                                        color: const Color(0xFFA2D9CE)),
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 15, horizontal: 30),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Text(
+                                            e.title,
+                                            style: const TextStyle(
+                                              color: Color(0xFF138D75),
+                                              fontWeight: FontWeight.w600,
+                                              fontSize: 17,
+                                            ),
+                                          ),
+                                          const Icon(
+                                            Icons.chevron_right_rounded,
+                                            color: Color(0xFF138D75),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                              ],
+                            );
+                          }
+                          return Container();
+                        },
+                      ),
+                    ],
+                  );
+                }
+                return const Text("...");
+              },
+            ),
+          ]),
         ),
-      ]),
+      ),
     );
   }
 }
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_detail_model.dart';
+import 'package:toonflix/models/webtoon_episode_model.dart';
 import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
@@ -18,6 +20,31 @@ class ApiService {
         webtoonList.add(WebtoonModel.fromJson(e));
       }
       return webtoonList;
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse("$baseUrl/$id");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodesById(
+      String id) async {
+    List<WebtoonEpisodeModel> episodeList = [];
+    final url = Uri.parse("$baseUrl/$id/episodes");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+      for (var e in episodes) {
+        episodeList.add(WebtoonEpisodeModel.fromJson(e));
+      }
+      return episodeList;
     }
     throw Error();
   }
